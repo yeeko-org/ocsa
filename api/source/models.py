c642178f6f2c31bd5606705d10452f8e72d9ca00
Lucian <vash512@users.noreply.github.com>
2024-04-27 03:55:18 -0600

    [api] se eliminaron los modelos legacy de las nuevas apps se eliminaron las migraciones y las configuraciones del admin

--- api/source/models.py
+++ api/source/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from project.models import Proyecto, EstatusProyecto, Project
-from space_time.models import Temporalidad, StatusProject
+from project.models import Project
+from space_time.models import StatusProject
 from work_flux.models import StatusControl
 
 
@@ -62,88 +62,3 @@ class Mention(models.Model):
     class Meta:
         verbose_name = 'Mención de proyecto en nota'
         verbose_name_plural = 'Menciones de proyectos en notas'
-
-
-# ============================================================================
-# ======================== VERSIÓN 1: ========================================
-# CREATE TABLE ocs.notas (
-#     id integer NOT NULL,
-#     id_nota integer,
-#     titulo text,
-#     autor text,
-#     nombre_medio text,
-#     pagina_medio text,
-#     vinculo text,
-#     fecha date,
-#     fecha_captura date
-# );
-
-class Nota(models.Model):
-    id_nota = models.IntegerField(blank=True, null=True)
-    titulo = models.TextField(blank=True, null=True)
-    autor = models.TextField(blank=True, null=True)
-    nombre_medio = models.TextField(blank=True, null=True)
-    pagina_medio = models.TextField(blank=True, null=True)
-    vinculo = models.TextField(blank=True, null=True)
-    fecha = models.DateField(blank=True, null=True)
-    fecha_captura = models.DateField(blank=True, null=True)
-
-    def __str__(self):
-        return self.titulo or str(self.pk)
-
-    class Meta:
-        verbose_name = 'Nota'
-        verbose_name_plural = 'Notas'
-        db_table = 'notas'
-
-
-# CREATE TABLE ocs.registro_notas (
-#     id integer NOT NULL,
-#     owner text,
-#     datum jsonb,
-#     status ocs.draft_status DEFAULT 'inprogress'::ocs.draft_status,
-#     last_edit timestamp with time zone DEFAULT CURRENT_TIMESTAMP
-# );
-
-
-# ALTER TABLE ocs.registro_notas ENABLE ROW LEVEL SECURITY;
-
-class RegistroNotas(models.Model):
-    owner = models.TextField(blank=True, null=True)
-    datum = models.JSONField(blank=True, null=True)
-    status = models.CharField(
-        max_length=100, default='inprogress', blank=True, null=True)
-    last_edit = models.DateTimeField(auto_now=True, blank=True, null=True)
-
-    def __str__(self):
-        return self.owner or str(self.pk)
-
-    class Meta:
-        verbose_name = 'Registro Nota'
-        verbose_name_plural = 'Registros Notas'
-        db_table = 'registro_notas'
-
-
-# CREATE TABLE ocs.estatus_proyectos (
-#     id integer NOT NULL,
-#     nota_id integer,  --ForeignKey
-#     proyecto_id integer NOT NULL,  --ForeignKey
-#     estatus_id integer,  --ForeignKey
-#     temporalidad_id integer  --ForeignKey
-# );
-
-# LUCIAN, hay que pasar esto a "source"
-class EstatusProyectos(models.Model):
-    nota = models.ForeignKey(Nota, on_delete=models.CASCADE)
-    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
-    estatus = models.ForeignKey(EstatusProyecto, on_delete=models.CASCADE)
-    temporalidad = models.ForeignKey(Temporalidad, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.nota
-
-    class Meta:
-        verbose_name = 'Estatus Proyecto'
-        verbose_name_plural = 'Estatus Proyectos'
-        db_table = 'estatus_proyectos'
-
