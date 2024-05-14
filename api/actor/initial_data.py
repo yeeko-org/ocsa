24d37ef726f49c779e0bf1bac37a7fb48cfc94f2
Lucian <vash512@users.noreply.github.com>
2024-05-14 04:18:25 -0600

    [api] initial_data

--- api/actor/initial_data.py
+++ api/actor/initial_data.py
@@ -1,6 +1,7 @@
+from work_flux.models import StatusControl
 from .models import (
-    init_participant_types, temporal_participant_types, ParticipantType,
-    init_sector_groups, SectorGroup)
+    Belong, Sector, init_participant_types, temporal_participant_types, ParticipantType,
+    init_sector_groups, init_sectors, SectorGroup, init_belongs)
 
 
 class ParticipantTypes:
@@ -32,4 +33,41 @@ class InitSectorGroups:
             )
 
 
+class InitSector:
+    def __init__(self):
+        for name, needs_name, sector_group_name, status_validation_name in init_sectors:
+            sector_group, _ = SectorGroup.objects.get_or_create(
+                name=sector_group_name)
 
+            if Sector.objects.filter(name=name).exists():
+                continue
+
+            if status_validation_name:
+                try:
+                    status_validation = StatusControl.objects.get(
+                        name=status_validation_name)
+                except StatusControl.DoesNotExist:
+                    status_validation = StatusControl.objects.create(
+                        name=status_validation_name,
+                        public_name=status_validation_name
+                    )
+            else:
+                status_validation = None
+
+            Sector.objects.create(
+                name=name,
+                needs_name=needs_name,
+                sector_group=sector_group,
+                status_validation=status_validation
+            )
+
+
+class InitBelongs:
+    def __init__(self):
+        for key_name, value in init_belongs:
+            if Belong.objects.filter(key_name=key_name).exists():
+                continue
+            Belong.objects.get_or_create(
+                key_name=key_name,
+                name=value
+            )
