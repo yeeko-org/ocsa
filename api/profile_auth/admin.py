16584464cef7f1a3b67fa3b66662146ca0554c92
Lucian <vash512@users.noreply.github.com>
2024-05-15 18:21:53 -0600

    [api] app para usuario

---
+++ api/profile_auth/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from profile_auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('phone',)}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
