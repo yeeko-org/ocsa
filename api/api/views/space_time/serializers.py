88276a269953954836c486fea2d687414b943893
Lucian <vash512@users.noreply.github.com>
2024-06-28 05:03:43 -0600

    [api] [YD-86] catalogos geograficos Space Time

---
+++ api/api/views/space_time/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+
+from space_time.models import (
+    Country,
+    State,
+    Municipality,
+    Locality,
+    Location,)
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+
+class LocalitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Locality
+        fields = '__all__'
+
+
+class MunicipalityListSerializer(serializers.ModelSerializer):
+    link = serializers.HyperlinkedIdentityField(
+        view_name='space_time_municipality-detail',
+        lookup_field='pk'
+    )
+
+    class Meta:
+        model = Municipality
+        fields = '__all__'
+
+
+class MunicipalityRetrieveSerializer(MunicipalityListSerializer):
+    localities = LocalitySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Municipality
+        fields = '__all__'
+
+
+class StateListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = State
+        fields = '__all__'
+
+
+class StateRetrieveSerializer(StateListSerializer):
+    municipalities = MunicipalityListSerializer(many=True, read_only=True)
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
