df54b71e430c43956579b5f6f4622a3a495b1a25
Ricardo <rickrebel@gmail.com>
2024-09-18 09:14:28 -0600

    [nuxt] dejo bien configurada la dirección de api

--- nuxt/store/common.js
+++ nuxt/store/common.js
@@ -6,7 +6,7 @@ export default axios.create({
   	? 'http://localhost:8000/api'
   	//? 'https://ocamis.ngrok.io/api'
     // ? 'https://arropa.yeeko.org/api'
-    : 'https://arropa.yeeko.org/api',
+    : 'https://apiocsa.yeeko.org/api',
   headers: {
     "Content-Type": "application/json"
     //"Authorization": "Token 1b38bcac3c4f57a1b57bf48a954111bd54a3f3cf"
