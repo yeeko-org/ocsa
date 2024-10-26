7679d42f75dca561670f780414e0cfd772211c37
Lucian <vash512@users.noreply.github.com>
2024-10-26 05:26:55 -0600

    [api] comando para migrar y asociar usuarios a notas mediante RegistroNotas

---
+++ api/profile_auth/management/commands/migrate_legacy_users.py
@@ -0,0 +1,59 @@
+from django.core.management.base import BaseCommand
+from django.utils.crypto import get_random_string
+
+from ocsa_legacy.models import RegistroNotas
+from profile_auth.models import User
+from source.models import Note
+
+
+class Command(BaseCommand):
+    help = "Migrate legacy users on RegistroNotas to new User model"
+
+    def handle(self, *args, **kwargs):
+        total = RegistroNotas.objects.count()
+        not_found = 0
+        for registro in RegistroNotas.objects.all():
+            owner = registro.owner
+            if not owner:
+                continue
+            user = self.create_user(owner)
+
+            datum = registro.datum
+            if not datum:
+                continue
+
+            title = datum.get("nota", {}).get("titulo")
+            if not title:
+                continue
+            notes_query = Note.objects.filter(title=title)
+            notes_count = notes_query.count()
+            if notes_count == 0:
+                self.stdout.write(
+                    self.style.ERROR(f"Note with title {title} not found"))
+
+                not_found += 1
+                continue
+            if notes_count > 1:
+                self.stdout.write(
+                    self.style.WARNING(
+                        f"Multiple notes({notes_count}) with title {title}"))
+            Note.objects.filter(title=title).update(editor=user)
+
+        self.stdout.write(
+            self.style.SUCCESS(f"Total: {total}, Not found: {not_found}"))
+
+    def create_user(self, email: str) -> User:
+        user, created = User.objects.get_or_create(
+            username=email,
+            email=email,
+            defaults={
+                "first_name": email.split("@")[0],
+                "last_name": "",
+            }
+        )
+        if created:
+            user.set_password(get_random_string(16))
+            user.save()
+            self.stdout.write(self.style.NOTICE(f"User {user} created"))
+
+        return user
